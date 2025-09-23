5d2c5bccfdcc392c8e9e154b44b68d4b8fde8572
Wilber García <71144997+1wilber@users.noreply.github.com>
2025-09-23 20:56:18 -0300

    feat: Simplificar formulario de transacciones

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def icon(name)
     render plain: Rails.root.join("app", "assets", "icons", "#{name}.svg").read.html_safe rescue nil
   end
+
+  def dollar_sign
+    "$"
+  end
+
+  def render_currency_flag(currency)
+    render plain: Rails.root.join("app", "assets", "flags", "#{currency}.svg").read.html_safe rescue "No existe #{currency}"
+  end
 end
